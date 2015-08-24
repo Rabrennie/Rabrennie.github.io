d2a0a8ff8f89c244b350bf15695b5157a80cb46f
Rab Rennie <RENNIERAB@gmail.com>
2015-08-25 00:30:11 +0100

    Change how the winning and achievement handling

--- youwin/js/app.js
+++ youwin/js/app.js
@@ -1,17 +1,29 @@
 var ywtgapp = angular.module("ywtgapp",[])
     .controller("ywtgController", function($scope, $interval){
+        var achievements = {
+            1:{title:"Winning",desc:"You have won the game!"},
+            100:{title:"More Winning",desc:"You have won some more!"},
+            250:{title:"Losing?",desc:"How did you just lose?"},
+            300:{title:"Won Again",desc:"Bro it was just a prank!"}
+        },
+        winStrings = {
+            1:"You win!",
+            100:"You win more!",
+            250:"You lose!",
+            300:"Just kidding, you win again!"
+        }
+
         $scope.youwin="You win!";
         $scope.counter=0;
-        $scope.notifications = [
-            {title:"Winning",desc:"You have won the game!"},
-        ]
+        $scope.notifications = [];
 
         $scope.gameLoop = function(){
             $scope.counter++;
-            if ($scope.counter === 100)
-            {
-                $scope.youwin = "You win more!";
-                $scope.notifications.push({title:"More winning",desc:"You have won the game more!"})
+            if (winStrings[$scope.counter]) {
+                $scope.youwin = winStrings[$scope.counter];
+            };
+            if (achievements[$scope.counter]) {
+                $scope.notifications.push(achievements[$scope.counter]);
             };
         }
 
